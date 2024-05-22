092626087af4c3bcc3b1cd5b55d8d771dccdef6d
Huang <jack237081@gmail.com>
2024-05-22 14:39:30 +0800

    form

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -8,11 +8,13 @@ namespace MSIT158Site.Controllers
     public class ApiController : Controller
     {
      private readonly MyDBContext _context;
-        public ApiController(MyDBContext context)
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+        public ApiController(MyDBContext context, IWebHostEnvironment hostEnvironment)
         {
             _context = context;
+            _hostEnvironment = hostEnvironment;
         }
-
 
 
         public IActionResult Index()
@@ -105,14 +107,46 @@ namespace MSIT158Site.Controllers
             }
         }
 
-        public IActionResult Register(MemberDTO member)
+        //public IActionResult Register(string userName, string email, int age = 20)
+        //IFormFile 是 ASP.NET Core 中用於處理檔案上傳的介面，avatar 參數用於接收使用者上傳的頭像檔案
+        public IActionResult Register(Member member, IFormFile avatar)
         {
-            if (string.IsNullOrEmpty(member.userName))
+            if (string.IsNullOrEmpty(member.Name))
             {
-                member.userName = "guest";
+                member.Name = "guest";
             }
-            return Content($"Hello {member.userName}，{member.Age} 歲了，電子郵件是 {member.Email}", "text/html", System.Text.Encoding.UTF8);
-        }
 
+            //取得上傳檔案的資訊
+            //string info = $"{avatar.FileName} - {avatar.Length} - {avatar.ContentType}";
+            //string info = _hostEnvironment.ContentRootPath;
+
+            //檔案上傳
+            //實際路徑
+            //string uploadPath = @"C:\Users\User\Documents\workspace\MSIT158Site\wwwroot\uploads\a.png";
+            //WebRootPath：C: \Users\User\Documents\workspace\MSIT158Site\wwwroot
+            //ContentRootPath：C:\Users\User\Documents\workspace\MSIT158Site
+
+            // 組合上傳路徑，將檔案儲存在 Web 根目錄下的 "uploads" 資料夾中
+            string uploadPath = Path.Combine(_hostEnvironment.WebRootPath, "uploads", avatar.FileName);
+            // 將上傳路徑存儲在 info 變數中，以供後續使用
+            string info = uploadPath;
+            // 使用檔案串流創建一個檔案，並將上傳的檔案資料寫入到該檔案中
+            //uploadPath 指定要創建的檔案的路徑 FileMode.Create:表示檔案的打開模式 如果指定的檔案不存在，就創建一個新的檔案
+            using (var fileStream = new FileStream(uploadPath, FileMode.Create))
+            {
+                //將從客戶端上傳的檔案avatar 資料複製到先前建立的路徑
+                avatar.CopyTo(fileStream); // 將上傳的檔案資料複製到創建的檔案串流中
+            }
+
+
+            return Content(info, "text/plain", System.Text.Encoding.UTF8);
+            //寫進資料庫
+            //_context.Members.Add(member);
+            //_context.SaveChanges();
+
+            // return Content($"Hello {member.Name}，{member.Age} 歲了，電子郵件是 {member.Email}", "text/html", System.Text.Encoding.UTF8);
+        }
     }
+
 }
+
