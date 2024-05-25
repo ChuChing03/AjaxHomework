aa559f420c193a05924e1ef00a5354ba21f4df84
Huang <jack237081@gmail.com>
2024-05-25 17:09:39 +0800

    hw3 hw4+

--- Controllers/HomeworkController.cs
+++ Controllers/HomeworkController.cs
@@ -1,14 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MSIT158Site.Models;
 
 namespace MSIT158Site.Controllers
 {
     public class HomeworkController : Controller
     {
+        private readonly ILogger<HomeworkController> _logger;
+        private readonly MyDBContext _context;
+        public HomeworkController(ILogger<HomeworkController> logger, MyDBContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
         public IActionResult Homework1()
         {
             return View();
         }
         public IActionResult Homework2()
+        {
+            return View();
+        }
+        public IActionResult Homework3()
+        {
+            return View();
+        }
+        public IActionResult Homework4()
         {
             return View();
         }
